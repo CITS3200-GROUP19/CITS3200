722642acaaf36a5cf65bd4ee813413173d5f9365
justinandronicos <justin.andronicos@gmail.com>
2019-10-08 23:22:05 +0800

    Added working admin page
    Admin page support for User model manipulation

--- FLASK_PROTOTYPE/FlaskDash/app/models.py
+++ FLASK_PROTOTYPE/FlaskDash/app/models.py
@@ -3,19 +3,19 @@ from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 
 from app.extensions import db
-from app.extensions import login
+# from app.extensions import login_manager
 
 
-@login.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# @login.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(80))
-    role = db.Column(db.Enum('doctor', 'researcher'))
+    role = db.Column(db.Enum('doctor', 'researcher', 'research'))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
