c5d9d98259a85db28366c7a18c068cd33ed89246
justinandronicos <justin.andronicos@gmail.com>
2019-10-09 14:32:19 +0800

    Heroku Test

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class BaseConfig: 
+class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ['SECRET_KEY']
     ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'https://flaskdash-deployment-test.herokuapp.com/']
+       
