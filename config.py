4a9cdf60d51dcfcb3c8014de1fcb4f30ff06a66f
justinandronicos <justin.andronicos@gmail.com>
2019-10-09 14:10:07 +0800

    Heroku Test

--- config.py
+++ config.py
@@ -2,14 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class BaseConfig:
-    FLASK_APP=dashapp
-    FLASK_ENV=development
-    DATABASE_URL=sqlite:///$PWD/app.db
-    SECRET_KEY=secret_key_change_as_you_wish_make_it_long_123
-    
+class BaseConfig: 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ['SECRET_KEY']
-
-
+    ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'https://flaskdash-deployment-test.herokuapp.com/']
