1e0120a40b562dbce24ab30579da925b2a976a5b
justinandronicos <justin.andronicos@gmail.com>
2019-10-09 14:35:45 +0800

    Heroku Test

--- config.py
+++ config.py
@@ -7,4 +7,3 @@ class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ['SECRET_KEY']
     ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'https://flaskdash-deployment-test.herokuapp.com/']
-       
